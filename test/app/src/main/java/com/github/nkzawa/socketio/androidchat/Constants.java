3c79fa877844ac89dfe709a0cc1ccd512361277d
Brobeibei <mac@wirelessprvnat-172-17-171-123.near.illinois.edu>
2015-10-18 20:06:55 -0500

    asdsa

--- test/app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
+++ test/app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -1,5 +1,5 @@
 package com.github.nkzawa.socketio.androidchat;
 
 public class Constants {
-    public static final String CHAT_SERVER_URL = "http://172.17.173.14:3000";
+    public static final String CHAT_SERVER_URL = "http://172.17.171.123:3000";
 }
